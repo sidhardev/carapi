4259d4d1e6a7d1706d2bbbf738ce4fb23d3ab083
sidhardev <your_email@example.com>
2025-12-19 16:35:57 +0530

    Database Connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Reports } from './reports/reports.entity';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [User, Reports],
+    synchronize: true,
+  }),
+    ReportsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,9 @@
+import { Column, Entity, PrimaryGeneratedColumn  } from "typeorm";
+
+@Entity() 
+export class Reports {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    price: number
+}
