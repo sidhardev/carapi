c5464e229e8d189680530c213b2e37a34e7df45d
sidhardev <your_email@example.com>
2025-12-23 16:08:32 +0530

    Created Signup api, and Find user by email, delete user by id and much more related to CURD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Query,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+@Controller('auth')
+export class UsersController {
+  constructor(private UsersService: UsersService) {}
+  @Post('/signup')
+  CreateUser(@Body() body: CreateUserDto) {
+    this.UsersService.create(body.email, body.password);
+  }
 
-@Controller('users')
-export class UsersController {}
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.UsersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.UsersService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.UsersService.remove(parseInt(id));
+    }   
+
+
+}
